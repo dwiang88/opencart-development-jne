745ca3fc2692e502344758d8bc32c9e1c01664cf
Gojay <dani.gojay@gmail.com>
2013-12-26 10:22:56 +0700

    init opencart

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-development-jne/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart-development-jne/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-development-jne/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart-development-jne/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart-development-jne/admin/');
+define('DIR_SYSTEM', '/var/www/opencart-development-jne/system/');
+define('DIR_DATABASE', '/var/www/opencart-development-jne/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart-development-jne/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart-development-jne/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart-development-jne/system/config/');
+define('DIR_IMAGE', '/var/www/opencart-development-jne/image/');
+define('DIR_CACHE', '/var/www/opencart-development-jne/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart-development-jne/download/');
+define('DIR_LOGS', '/var/www/opencart-development-jne/system/logs/');
+define('DIR_CATALOG', '/var/www/opencart-development-jne/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', ' opencart-development');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-development-jne/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-development-jne/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart-development-jne/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart-development-jne/system/');
+define('DIR_DATABASE', '/var/www/opencart-development-jne/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart-development-jne/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencart-development-jne/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencart-development-jne/system/config/');
+define('DIR_IMAGE', '/var/www/opencart-development-jne/image/');
+define('DIR_CACHE', '/var/www/opencart-development-jne/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart-development-jne/download/');
+define('DIR_LOGS', '/var/www/opencart-development-jne/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', ' opencart-development');
+define('DB_PREFIX', 'oc_');
+?>
